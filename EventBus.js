691be3981e55f9f942b630a7fa1ea57460ca5d65
Foxawy <xfoxawy@gmail.com>
2018-11-06 01:15:40 -0500

    [master] first ver

---
+++ EventBus.js
@@ -0,0 +1,96 @@
+/**
+ * Author: xfoxawy@gmail.com
+ */
+
+/**
+ * EventBus Pub/sub Mediator Module
+ */
+var EventBus = function(){
+    var __ = {};
+    this.channels = [];
+    this.channel_subscribers = [];
+    this.events_subscribers = [];
+};
+
+/**
+ * [pushEvent description]
+ * @param  {[type]} channel [description]
+ * @param  {[type]} event   [description]
+ * @param  {[type]} data    [description]
+ * @return {[type]}         [description]
+ */
+EventBus.prototype.pushEvent = function(channel, event, data){
+
+        if(!this.channels[channel]) this.channels[channel] = {};
+        
+        this.channels[channel][event] = {
+            data : data
+        };
+
+        if(
+            this.channel_subscribers[channel] 
+            &&this.channel_subscribers[channel].length
+        ){
+            this.channel_subscribers[channel].forEach(function(cb){
+                cb(event, data);
+            });
+        }
+
+        if(
+            this.events_subscribers[channel] 
+            &&this.events_subscribers[channel][event] 
+            &&this.events_subscribers[channel][event].length
+        ){
+            this.events_subscribers[channel][event].forEach(function(cb){
+                cb(event, data);
+            });
+        }
+};
+
+/**
+ * [subscribeToChannel description]
+ * @param  {[type]} channel [description]
+ * @param  {[type]} listner [description]
+ * @return {[type]}         [description]
+ */
+EventBus.prototype.subscribeToChannel = function(channel, listner){
+    if(typeof listner !== 'function') throw new Error('can\'t register uncallable');
+    if(!this.channel_subscribers[channel]) this.channel_subscribers[channel] = [];
+    this.channel_subscribers[channel].push(listner);
+
+    // send old events
+    if(
+        this.channels[channel] 
+        &&Object.keys(this.channels[channel]).length
+    ){
+        var keys = Object.keys(this.channels[channel]);
+        for(var i = 0; i < keys.length; i++){
+            listner(keys[i], this.channels[channel][keys[i]].data);
+        }
+    }
+};
+
+/**
+ * [subscribeToEvent description]
+ * @param  {[type]} channel [description]
+ * @param  {[type]} event   [description]
+ * @param  {[type]} listner [description]
+ * @return {[type]}         [description]
+ */
+EventBus.prototype.subscribeToEvent = function(channel, event, listner){
+    if(typeof listner !== 'function') throw new Error('can\'t register uncallable');
+    if(!this.events_subscribers[channel]) this.events_subscribers[channel] = [];
+    if(!this.events_subscribers[channel][event]) this.events_subscribers[channel][event] = [];
+    this.events_subscribers[channel][event].push(listner);
+
+    // send old events
+    if(
+        this.channels[channel] 
+        &&this.channels[channel][event]
+        &&Object.keys(this.channels[channel][event]).length
+    ){
+        listner(event, this.channels[channel][event].data);
+    }
+};
+
+module.exports = EventBus;
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# eventbusjs
-Simple Pub/Sub Event Bus as mediator pattern
+## EventBusJs
+Simple Pub/Sub Event Bus as mediator pattern, it can be used as mediator module to communicate between different modules, its easy to use.
+Inspired by Laravel EventBus
+
+### Installation
+
+    npm install eventbusjs
+
+### Usage
+
+ 
+
+ - Import `var EventBus = require('eventbusjs');`
+ - create new instance `let eb = new EventBus();`
+ - Subscribe to a channel `eb.subscribeToChannel('test_channel', my_cb)`
+ - Subscribe to specific event on a channel `eb.subscribeToEvent('test_channel', 'test_event', my_cb)`
+ - Push events  `eb.pushEvent('test_channel', 'test_event', {data : 'success'})` and it will automatically invoke subscribed callbacks.
+ - 
